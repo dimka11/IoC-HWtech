27a944a2069d5967f4bd063f14fb9ede081ab05a
etyumentcev <etyumentcev@gmail.com>
2018-08-06 10:23:36 -0700

    initialized ioc-core project

---
+++ IoC-core/src/main/kotlin/HWdTech/Resolve.kt
@@ -0,0 +1,7 @@
+package HWdTech.IoC
+
+import kotlin.TODO
+
+fun <T> resolve (key: Any, vararg args: Any ): T {
+    TODO ("IoC.resolve should be implemented.")
+}
---
+++ IoC-core/src/test/kotlin/HWdTech/ResolveTests.kt
@@ -0,0 +1,8 @@
+package HWdTech.IoC
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class resolveTests {
+
+}
