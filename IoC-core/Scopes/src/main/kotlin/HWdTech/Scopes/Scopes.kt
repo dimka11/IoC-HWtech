075372698d3ede5e9a294645e5b7f1330fb82eb5
etyumentcev <etyumentcev@gmail.com>
2018-08-07 19:57:02 -0700

    Initial implementation of scope has been done

---
+++ IoC-core/Scopes/src/main/kotlin/HWdTech/Scopes/Scopes.kt
@@ -0,0 +1,46 @@
+package HWdTech.IoC
+
+import kotlin.concurrent.getOrSet
+
+open class Scopes {
+    companion object {
+        private val scopesInvoker: ThreadLocal<IScope> = ThreadLocal<IScope>()
+
+        var current: IScope
+            get() {
+                return scopesInvoker.getOrSet {
+                    newScope()
+                }
+            }
+            set(value) {
+                scopesInvoker.set(value)
+            }
+
+        fun startNew(): IScope {
+            var nextScope = { s: IScope -> newScope(s) }
+
+            val currentScope = scopesInvoker.getOrSet {
+                nextScope = { s: IScope -> s }
+                return newScope()
+            }
+
+            current = nextScope(currentScope)
+
+            return current
+        }
+
+        private fun newScope(parentScope: IScope): IScope {
+            return ScopeImpl({ current = parentScope })
+        }
+
+        private fun newScope(): IScope {
+            return ScopeImpl({ remove() })
+        }
+
+        internal fun remove() {
+            scopesInvoker.remove()
+        }
+    }
+}
+
+
--- IoC-core/Scopes/src/test/kotlin/HWdTech/ScopesTests.kt
+++ IoC-core/Scopes/src/test/kotlin/HWdTech/ScopesTests.kt
@@ -1,3 +1,36 @@
-package HWdTech
+package HWdTech.IoC
 
-class ScopesTests
+import kotlin.test.assertNotNull
+import kotlin.test.assertNotSame
+import kotlin.test.assertSame
+import org.junit.Test as test
+
+class `Scopes tests` {
+    @test
+    fun `Current scope is not null always`() {
+        assertNotNull(Scopes.current, "Current scope should be not null.")
+    }
+
+    @test
+    fun `New scope created by startNew must be a not null`() {
+        Scopes.startNew().use {
+            assertNotNull(it, "Created scope should be not null.")
+            assertSame(it, Scopes.current, "Created scope should be a current scope.")
+        }
+    }
+
+    @test
+    fun `use should return back to previous scope`() {
+        Scopes.startNew().use {
+            val parentScope = it
+            assertSame(it, Scopes.current, "Created scope should be a current scope.")
+
+            Scopes.startNew().use {
+                assertNotSame(parentScope, it, "Created scope should be different from previous")
+                assertSame(it, Scopes.current, "Created new scope should be a current scope.")
+            }
+
+            assertSame(it, Scopes.current, "Current scope should be equal to parent scope")
+        }
+    }
+}
